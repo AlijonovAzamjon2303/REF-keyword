a4771a4d69a6b86ea2c19685da027777d431bc5a
alijo <alijo@ALIJONOV>
2023-11-20 11:54:57 +0500

    Add project files.

---
+++ REF keyword/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Aka
+{
+    public static void Main()
+    {
+        int n = 123, cnt = 0, sum = 0;
+        sumOfDigits(n, ref cnt, ref sum);
+        Console.WriteLine($"{cnt} xonali son raqamlari yig'indisi {sum}");
+    }
+
+    public static void sumOfDigits(int n, ref int count, ref int sum)
+    {
+        while(n > 0)
+        {
+            sum += n % 10;
+            n /= 10;
+            count++;
+        } 
+    }
+}
